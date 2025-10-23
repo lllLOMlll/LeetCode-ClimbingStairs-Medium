407bde897ece89637aa683dc13ac33de9932e21a
Louis-Olivier Major <louis.olivier.major@gmail.com>
2025-10-23 07:44:32 -0400

    Working up to 5

--- Program.cs
+++ Program.cs
@@ -7,14 +7,16 @@
 
         int distinctWays = 0;
         int divisionRemainder = n / 2;
+        int addingToTheLoop = (n % 2 == 0) ? 0 : 1;
 
-        for (int i = 1; i < divisionRemainder + 1; i++)
+        for (int i = 1; i < divisionRemainder + addingToTheLoop; i++)
         {
             distinctWays += n - i;
         }
 
+        distinctWays += (n % 2 == 0) ? 2 : 1;
         // Add 1 for the only ones solution
-        distinctWays++;
+        //distinctWays++;
 
         return distinctWays;
     }
@@ -22,19 +24,18 @@
     static void Main(string[] args)
     {
         Solution s = new Solution();
-        // int restant = 5/2;
-        // Console.WriteLine("Restant de 5/2 = " + restant);
-        // int input1 = 1;
-        // Console.WriteLine("Input = 1 Answer(2) -> " + s.ClimbStairs(input1));
 
-        // int input2 = 2;
-        // Console.WriteLine("Input = 2 Answer(2) -> " + s.ClimbStairs(input2));
+        int input1 = 1;
+        Console.WriteLine("Input = 1 Answer(1) -> " + s.ClimbStairs(input1));
 
-        // int input3 = 3;
-        // Console.WriteLine("Input = 3 Answer(3) -> " + s.ClimbStairs(input3));
+        int input2 = 2;
+        Console.WriteLine("Input = 2 Answer(2) -> " + s.ClimbStairs(input2));
 
-        // int input4 = 4;
-        // Console.WriteLine("Input = 4 Answer(5) -> " + s.ClimbStairs(input4));
+        int input3 = 3;
+        Console.WriteLine("Input = 3 Answer(3) -> " + s.ClimbStairs(input3));
+
+        int input4 = 4;
+        Console.WriteLine("Input = 4 Answer(5) -> " + s.ClimbStairs(input4));
 
         int input5 = 5;
         Console.WriteLine("Input = 5 Answer(8) -> " + s.ClimbStairs(input5));
--- obj/Debug/net9.0/70_ClimbingStairs.AssemblyInfo.cs
+++ obj/Debug/net9.0/70_ClimbingStairs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("70_ClimbingStairs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18418ddcc7f7bb057fed5ae355548e9a40dae01e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d178fbd085830448b870fafb544051b5f75459e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("70_ClimbingStairs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("70_ClimbingStairs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
